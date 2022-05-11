f57349bb1652726f155c9af8bfa3874ad2778ac7
Arnisfet <mrudge@student.21-school.ru>
2022-05-11 23:36:37 +0300

    Начинаю реализацию реверс итератора

--- includes/ft_type_traits.h
+++ includes/ft_type_traits.h
@@ -7,6 +7,58 @@
 
 
 #include <iostream>
+#include <cstddef>
+
+namespace ft {
+	/* Iterator Traits */
+	template <class Iterator>
+			class iterator_traits
+	{
+	public:
+		typedef typename Iterator::difference_type difference_type;
+		typedef typename Iterator::value_type value_type;
+		typedef typename Iterator::pointer pointer;
+		typedef typename Iterator::reference reference;
+		typedef typename Iterator::iterator_category iterator_category;
+	};
+
+	/* T* specialization: */
+	template <class T>
+			class iterator_traits <T *>
+	{
+	public:
+		typedef ptrdiff_t diffrence_type;
+		typedef T value_type;
+		typedef T *pointer;
+		typedef T &reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
+
+	/* const T* specialization: */
+	template <class T>
+	class iterator_traits <const T *>
+	{
+	public:
+		typedef ptrdiff_t diffrence_type;
+		typedef T value_type;
+		typedef const T *pointer;
+		typedef const T &reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
+
+	template <class Iterator>
+	class ReverseIterator
+	{
+	public:
+		typedef Iterator												iterator_type;
+		typedef typename iterator_traits<Iterator>::iterator_category	iterator_category;
+		typedef typename iterator_traits<Iterator>::value_type			value_type;
+		typedef typename iterator_traits<Iterator>::difference_type		difference_type;
+		typedef typename iterator_traits<Iterator>::pointer				pointer;
+		typedef typename iterator_traits<Iterator>::reference			reference;
+	};
+}
+
 
 namespace ft {
 
