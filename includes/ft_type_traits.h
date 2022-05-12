a9342b6283e1956dad0c21320930162b000fb4fb
Arnisfet <mrudge@student.21-school.ru>
2022-05-12 23:57:48 +0300

    Start make classic iterator

--- includes/ft_type_traits.h
+++ includes/ft_type_traits.h
@@ -47,6 +47,24 @@ namespace ft {
 	};
 
 	template <class Iterator>
+	class VectorIterator
+	{
+	public:
+
+		typedef typename ft::iterator_traits<Iterator>::value_type 			value_type;
+		typedef typename ft::iterator_traits<Iterator>::reference 			reference;
+		typedef typename ft::iterator_traits<Iterator>::pointer				pointer;
+		typedef typename ft::iterator_traits<Iterator>::difference_type		difference_type;
+		typedef typename ft::iterator_traits<Iterator>::iterator_category	iterator_category;
+		typedef typename ft::iterator_traits<Iterator>::pointer				iterator_type;
+	private:
+		pointer _p;
+	public:
+
+	};
+
+
+	template <class Iterator>
 	class ReverseIterator
 	{
 	public:
